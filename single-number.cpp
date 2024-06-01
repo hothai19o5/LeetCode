93db9763aeedd2851503470665bf82a9fb19be7c
hothai19o5 <supperbroku@gmail.com>
2024-06-02 00:48:33 +0700

    single number

---
+++ single-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result = nums[0];
+        for(int i = 1; i < n; i++){
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
