b2d7a47596f370c76931aa4e9f47aa76301ad27e
hothai19o5 <supperbroku@gmail.com>
2024-06-11 21:04:48 +0700

    maximum sub array divide and conquer

--- maxium-subarray.cpp
+++ maxium-subarray.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-class Solution1 { // O(n)
+class Solution1 { // Kadane 
 public:
     int maxiumSubarray(vector<int> &nums){
         int sum1 = 0;
@@ -15,5 +16,28 @@ public:
         }
         return sum2;
     }
+};
 
+class Solution2 {   // divide and conquer
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, nums.size()-1);
+    }
+    int maxSubArray(vector<int>& nums, int L, int R) {
+        if(L > R) return INT_MIN;
+
+        int leftSum = 0, rightSum = 0, curSum, mid = (L + R)/2;
+
+        for(int i = mid - 1, curSum = 0; i >= L; i--) {
+            curSum += nums[i];
+            leftSum = max(curSum, leftSum);
+        }
+
+        for(int i = mid + 1, curSum = 0; i <= R; i++) {
+            curSum += nums[i];
+            rightSum = max(curSum, rightSum);
+        }
+
+        return max({maxSubArray(nums, L, mid - 1), maxSubArray(nums, mid + 1, R), leftSum + nums[mid] + rightSum});
+    }
 };
